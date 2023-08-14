7df85afbc2d9d54d49a228bf75588f6c8a20ba7c
Abdullah AKALIN <abdullah.akalin@technarts.com>
2023-08-14 20:16:58 +0300

    Removed transpiled js files.

--- src/pkg/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useApiReporter = exports.useApi = void 0;
-var useApi_1 = require("./useApi");
-exports.useApi = useApi_1.default;
-var useApiReporter_1 = require("./useApiReporter");
-exports.useApiReporter = useApiReporter_1.default;
