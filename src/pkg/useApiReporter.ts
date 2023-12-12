83aadf8040f40c12ae6ce57c55e18748148a067e
Ayşe Kelleci <70059998+aysekelleci@users.noreply.github.com>
2023-12-12 10:37:57 +0300

    support multi apicounsels in apiReporter

--- src/pkg/useApiReporter.ts
+++ src/pkg/useApiReporter.ts
@@ -3,7 +3,7 @@ import * as React from "react";
 import { ApiCounsel } from "./types";
 
 type Params<T> = {
-  apiCounsel: ApiCounsel<T>,
+  apiCounsel: ApiCounsel<T>[] | ApiCounsel<T>,
   start?: () => void,
   end?: (
     resp: ApiCounsel<T>["RESP"],
@@ -13,19 +13,47 @@ type Params<T> = {
 }
 
 export default function useApiReporter<T>(params: Params<T>) {
-  const [isStarted, setStarted] = React.useState(false);
+  let apiCounsels: ApiCounsel<T>[];
+  if(Array.isArray(params.apiCounsel)) {
+    apiCounsels = params.apiCounsel
+  }
+  else {
+    apiCounsels = [params.apiCounsel]
+  }
+
+  const [isStarted, setStarted] = React.useState<boolean[]>(new Array(apiCounsels.length).fill(false));
 
   React.useEffect(() => {
-    if (params.apiCounsel.inFlight) {
-      params.start?.();
-      setStarted(true);
-    } else if (isStarted && !params.apiCounsel.inFlight) {
-      params.end?.(
-        params.apiCounsel.RESP,
-        params.apiCounsel.error,
-        params.apiCounsel.fault
-      );
-      setStarted(false);
-    }
-  }, [params.apiCounsel.inFlight, isStarted])
+    let isMounted = true;
+
+    apiCounsels.forEach((element, index) => {
+      if (element.inFlight && !isStarted[index]) {
+        params.start?.();
+        setStarted(prevState => {
+          if (isMounted) {
+            const newState = [...prevState];
+            newState[index] = true;
+            return newState;
+          }
+          return prevState;
+        });
+      } else if (isStarted[index] && !element.inFlight) {
+        params.end?.(
+          element.RESP,
+          element.error,
+          element.fault
+        );
+        setStarted(prevState => {
+          if (isMounted) {
+            const newState = [...prevState];
+            newState[index] = false;
+            return newState;
+          }
+          return prevState;
+        });
+      }
+    });
+  }, [params.apiCounsel, isStarted]);
 }
+
+
